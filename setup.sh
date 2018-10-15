1c999cb17ae6b59da8fd060dc0a8a740459b72f4
Tianyu (Terry) Chen <tianyuc@google.com>
2018-10-15 14:58:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Git
-sudo apt-get install git -y
+sudo apt-get install git-all -y
 
 ######################################
 #
