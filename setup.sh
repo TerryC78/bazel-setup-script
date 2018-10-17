64be559da64d42f1f7e21467e9a5565c3dfc045c
Tianyu (Terry) Chen <tianyuc@google.com>
2018-10-17 15:24:06 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ chmod +x bazel-0.17.2-installer-linux-x86_64.sh
 ./bazel-0.17.2-installer-linux-x86_64.sh --user
 export PATH="$PATH:$HOME/bin"
 echo 'export PATH="$PATH:$HOME/bin"' >> ~/.zshrc
-echo 'export PATH="$PATH:$HOME/bin"' >> ~/.basshrc
+echo 'export PATH="$PATH:$HOME/bin"' >> ~/.bashrc
 source ~/.bashrc
 
 # sudo apt-get install -y clang-format-7
